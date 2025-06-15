f045f0319e5b9bd0e8fd17a9d7307f996ac8726f
Shenandoah-Walker <saw67402@email.vccs.edu>
2025-06-15 02:33:26 +0000

    Change middle variable back to original value

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -7,7 +7,7 @@ int main()
   char first, middle, last;
   //Store your initials in these variables
   first = 'S';
-  middle = 'AA';
+  middle = 'A';
   last = 'W';
 
   cout << first << middle << last;
