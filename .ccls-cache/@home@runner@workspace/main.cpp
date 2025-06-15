6067e631defe434addb63b721b2666a27b8d0f67
Shenandoah-Walker <saw67402@email.vccs.edu>
2025-06-15 02:32:15 +0000

    Add an extra character to the middle variable

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -7,7 +7,7 @@ int main()
   char first, middle, last;
   //Store your initials in these variables
   first = 'S';
-  middle = 'A';
+  middle = 'AA';
   last = 'W';
 
   cout << first << middle << last;
